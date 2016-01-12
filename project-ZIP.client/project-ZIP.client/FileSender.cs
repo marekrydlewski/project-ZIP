36ecc999dd17a991fd67069626ee1035a842638c
Marcin Jabłoński <jablon1994@gmail.com>
2016-01-12 18:56:09 +0100

    Split class into file and directory sender

--- project-ZIP.client/project-ZIP.client/FileSender.cs
+++ project-ZIP.client/project-ZIP.client/FileSender.cs
@@ -1,46 +1,21 @@
-﻿using System.Collections.Generic;
-using System.IO;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace project_ZIP.client
 {
-    public class DirectorySender
+    public class FileSender
     {
-        public enum  SendDirectoryStatus
+        public enum SendFileStatus
         {
             OK, ERROR
         }
 
-        public static SendDirectoryStatus SendDirectory(string path, out Dictionary<string, string> newFileMap, Dictionary<string, string> fileMap = null, string parentDirectory = "")
+        public static SendFileStatus SendFile(string path)
         {
-            if (fileMap == null) fileMap = new Dictionary<string, string>();
-
-            string dirName = Path.Combine(parentDirectory, Path.GetFileName(path));
-
-            string[] files = Directory.GetFiles(path);
-            string[] subdirectories = Directory.GetDirectories(path);
-
-            //send files
-
-            foreach (var fileName in files)
-            {
-                fileMap.Add(Path.Combine(dirName, Path.GetFileName(fileName)), dirName);
-            }
-
-            foreach (var subdirectoryPath in subdirectories)
-            {
-                if (SendDirectory(subdirectoryPath, out newFileMap, fileMap, dirName) == SendDirectoryStatus.OK)
-                {
-                    fileMap = newFileMap;
-                }
-                else
-                {
-                    newFileMap = null;
-                    return SendDirectoryStatus.ERROR;
-                }
-            }
-            newFileMap = fileMap;
-            return SendDirectoryStatus.OK;
+            return SendFileStatus.OK;
         }
     }
 }
