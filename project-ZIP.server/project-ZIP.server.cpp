8fc486b5b29b7e5772eceb9fdb5424fed67c77fd
Marek Rydlewski <marek.rydlewski@hotmail.com>
2016-01-14 01:22:51 +0100

    Creation of test file

--- project-ZIP.server/project-ZIP.server.cpp
+++ project-ZIP.server/project-ZIP.server.cpp
@@ -3,7 +3,7 @@
 #include "ZipArchive.h"
 #include "ServerZip.h"
 #include <iostream>
-
+using namespace source;
 int main(int argc, char **argv) {
 
 	///test
@@ -11,16 +11,17 @@ int main(int argc, char **argv) {
 	server = ServerZip::getInstance();
 
 	try {
-		ZipArchive archive{"mydata.zip"};
-		ZipStat stat = archive.stat("README");
-		ZipFile file = archive.open("README");
+	ZipArchive archive{"output-new.zip", ZIP_CREATE};
 
-		std::cout << "content of README:" << std::endl;
-		std::cout << file.read(stat.size);
-	} catch (const std::exception &ex) {
-		std::cerr << ex.what() << std::endl;
-		std::exit(1);
-	}
+	// Add a buffer to the base archive
+	archive.add(Buffer{"Data from buffer"}, "documentFromBuffer");
+
+	// Add a file with all its contents to the directory assets/ in the archive
+	archive.addDirectory("assets");
+} catch (const std::exception &ex) {
+	std::cerr << ex.what() << std::endl;
+	std::exit(1);
+}
 	///
     server->connect();
 
