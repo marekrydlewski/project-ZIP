4f3038e20eff40251b94bb832449b6eafbf629e8
Marek Rydlewski <marek.rydlewski@hotmail.com>
2016-01-14 02:18:41 +0100

    Super duper buidl sh

--- project-ZIP.server/build.sh
+++ project-ZIP.server/build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 cd ${0%/*}
-g++ -std=c++14 ./ServerZip.cpp ./ZipArchive.cpp ./project-ZIP.server.cpp -I/usr/local/lib/libzip/include -lzip -pthread -o ./server -Wall
-./server
+
+if [[ $1 = debug ]]; then
+    g++ -std=c++14 ./ServerZip.cpp ./ZipArchive.cpp ./project-ZIP.server.cpp -I/usr/local/lib/libzip/include -lzip -pthread -o ./server -Wall -g
+    gdb ./server
+    #gdb run
+    #gdb brake $2:$3
+else
+    g++ -std=c++14 ./ServerZip.cpp ./ZipArchive.cpp ./project-ZIP.server.cpp -I/usr/local/lib/libzip/include -lzip -pthread -o ./server -Wall
+    ./server
+fi
+
