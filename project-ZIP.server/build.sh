749d74e37942cd3c1b9a0c398f1579715ca006de
Marek Rydlewski <marek.rydlewski@hotmail.com>
2016-01-15 01:33:54 +0100

    Clean bash script

--- project-ZIP.server/build.sh
+++ project-ZIP.server/build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-cd ${0%/*}
 
-if [[ $1 = debug ]]; then
+cd ${0%/*}
+if test "$1" == "debug"
+then
     g++ -std=c++14 ./ServerZip.cpp ./ZipArchive.cpp ./project-ZIP.server.cpp -I/usr/local/lib/libzip/include -lzip -pthread -o ./server -Wall -g
     gdb ./server
-    #gdb run
-    #gdb brake $2:$3
 else
     g++ -std=c++14 ./ServerZip.cpp ./ZipArchive.cpp ./project-ZIP.server.cpp -I/usr/local/lib/libzip/include -lzip -pthread -o ./server -Wall
     ./server
