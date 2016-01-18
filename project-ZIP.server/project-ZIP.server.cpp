71249e7eb2dae36096989ddc395c1faf4261ca7f
Marek Rydlewski <marek.rydlewski@hotmail.com>
2016-01-19 00:25:30 +0100

    Catching exceptions && ultra catchy handling

--- project-ZIP.server/project-ZIP.server.cpp
+++ project-ZIP.server/project-ZIP.server.cpp
@@ -8,6 +8,11 @@ int main(int argc, char **argv) {
 
 	ServerZip* server;
 	server = ServerZip::getInstance();
-    server->connect();
+    try {
+        server->connect();
+    }
+    catch (const std::exception& e) {
+        std::cerror<<"Error: "<<e.what()<<endl;
+    }
 
 }
